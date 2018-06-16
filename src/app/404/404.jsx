1fc1e83444290c820c73c2c225b7fe3c28effa89
Gethyn Jones <gethyn1@gmail.com>
2018-06-16 18:07:12 +0100

    Fix links on 404 page

--- src/app/404/404.jsx
+++ src/app/404/404.jsx
@@ -5,6 +5,7 @@ import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom'
 import Container from 'common/components/container'
 import { Layout, LayoutItem } from 'common/components/layout'
+import { PRODUCERS_PATH, CONTACT_PATH } from 'common/constants/paths'
 
 type Props = {
   trackPageNotFound: Function,
@@ -37,7 +38,7 @@ class NotFound extends React.Component<Props> {
               <div className="u-padding-vertical-xlg u-text-center s-body">
                 <h1 className="u-h1">Uh-oh!</h1>
                 <p>The page you are looking for doesn&#39;t exist ...</p>
-                <p>You can browse your <Link to="/">local producers</Link>, find out more <Link to="/about-the-app">about the app</Link> or register your interest on our <Link to="/signup">signup page</Link></p>
+                <p>You can browse your <Link to={PRODUCERS_PATH}>local producers</Link> or ask us a question on our <Link to={CONTACT_PATH}>contact page</Link>.</p>
               </div>
             </LayoutItem>
           </Layout>
